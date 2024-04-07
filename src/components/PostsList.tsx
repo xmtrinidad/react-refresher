493968f3fd0e4701f824566aac3c53fda6130249
Xavier <xmtrinidad@gmail.com>
2024-04-07 14:58:21 -0700

    router updates

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -2,13 +2,35 @@
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from './PostsList.module.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Post from "./Post";
 
 function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState<any[]>([]);
+  const [isFetching, setIsFetching] = useState(false);
+
+  useEffect(() => {
+
+    async function fetchPosts() {
+      setIsFetching(true);
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+      setIsFetching(false);
+    }
+
+    fetchPosts();
+
+  }, []);
 
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
     setPosts((existingPosts) => [...existingPosts, postData]);
   }
 
@@ -29,6 +51,7 @@ function PostList({ isPosting, onStopPosting }) {
           ))}
         </ul>
       )}
+      {isFetching && <p>Loading Posts...</p>}
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './index.css'
+import NewPost from './components/NewPost.tsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: '/create-post', element: <NewPost /> }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
